64c7d90b2c472b199d7e034bb9b8b2f99ca62669
leke <lekedevrepo@gmail.com>
2021-02-01 15:47:19 -0500

    add product image

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Sock',
+            image: './assets/images/socks_green.jpg',
             description: 'This is a good sock for your winter'
         }
     }
