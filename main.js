022b8d99fc8ba91357a2170126557c29cc9cdbb6
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-03-26 08:26:51 +0100

    add watch

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Sock',
-            image: './assets/images/socks_green.jpg',
-            description: 'This is a good sock for your winter',
-            url: 'https://homefinderapp.netlify.app/'
-        }
-    }
-})
+  data() {
+    return {
+      product: 'Sock',
+      image: './assets/images/socks_green.jpg',
+      description: 'This is a good sock for your winter',
+      url: 'https://homefinderapp.netlify.app/',
+      a: 'This is a property',
+      b: 'This is b property',
+    };
+  },
+
+  created() {
+    this.$watch('a', (newVal, oldVal) => {
+      console.log('This is new value', newVal);
+      console.log('This is old value', oldVal);
+    });
+  },
+});
